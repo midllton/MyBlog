1dead4638097b1028e89321a7d0be99d27e1ce8f
midllton <fb135246789@gmail.com>
2022-09-18 22:42:48 +0800

    test contributions

--- server/routes/index.js
+++ server/routes/index.js
@@ -7,10 +7,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/getNavMenu', getNavMenu) //获取菜单
-router.get('/getFooter', getFooter) //获取底部内容
-router.get('/getLinks', getLinks) //获取友情链接内容
-router.get('/getIndexPic', getIndexPic) //获取首页轮播内容
-router.get('/getHotArticle', getHotArticle) //获取首页轮播内容
+router.get('/getNavMenu', getNavMenu)  //获取菜单
+router.get('/getFooter', getFooter)   //获取底部内容
+router.get('/getLinks', getLinks)    //获取友情链接内容
+router.get('/getIndexPic', getIndexPic)   //获取首页轮播内容
+router.get('/getHotArticle', getHotArticle)  //获取首页轮播内容
 
 module.exports = router;
